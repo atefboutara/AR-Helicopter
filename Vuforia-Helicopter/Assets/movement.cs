dcf3540854d98fb14f9b3db2ae19674d0aed89ba
atefboutara <atef.boutara.98@gmail.com>
2019-06-17 20:58:51 +0100

    VuforiaHelico Uploaded

---
+++ Vuforia-Helicopter/Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Joystick MovementButton;
+    public Joystick VerticalButton;
+    Rigidbody rg;
+    void Start()
+    {
+        rg = gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rg.velocity = new Vector3(MovementButton.Horizontal * 1.0f, VerticalButton.Vertical * 1.0f, MovementButton.Vertical * 1.0f);
+    }
+}
